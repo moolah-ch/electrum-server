abc16d4dc52b788adeaba5c1517e2150fe9725c5
ThomasV <thomasv@gitorious>
2013-05-28 21:31:40 +0400

    m

--- backends/bitcoind/storage.py
+++ backends/bitcoind/storage.py
@@ -347,6 +347,9 @@ class Storage(object):
         else:
             skip_string = ''
 
+        if skip_string:
+            print "hashing skip string", skip_string.encode('hex')
+
         _hash = self.hash( skip_string + ''.join(hashes) )
 
         return _hash, value
